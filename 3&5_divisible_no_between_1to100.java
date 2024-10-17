ee3a8f93f3f85376e02f43685e786c38c1c33195
Naveenkumar96267 <161093045+Naveenkumar96267@users.noreply.github.com>
2024-10-17 22:08:29 +0530

    Add files via upload

---
+++ 3&5_divisible_no_between_1to100.java
@@ -0,0 +1,9 @@
+public class 3&5_divisible_no_between_1to100 {
+    public static void main(String[] args) {
+        for(int i =1;i<=100;i++){
+            if(i%3==0&i%5==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
